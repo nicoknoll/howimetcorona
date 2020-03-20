d83c8dfcb78789f50329159ea3557d1b3fe20762
Nico Knoll <mail@nico.is>
2020-03-20 22:12:05 +0100

    Add model

---
+++ core/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class VisitedPoint(BaseModel):
+    lat = models.DecimalField(max_digits=10, decimal_places=7)
+    lng = models.DecimalField(max_digits=10, decimal_places=7)
+    visited_at = models.DateTimeField()
+    is_verified = models.BooleanField(default=False)
