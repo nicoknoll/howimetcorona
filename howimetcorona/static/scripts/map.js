6e7dd27b542495e0770b77aab753c2ee4a6018ac
Nico Knoll <mail@nico.is>
2020-03-21 00:44:41 +0100

    Add center of the world

--- howimetcorona/static/scripts/map.js
+++ howimetcorona/static/scripts/map.js
@@ -1,14 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var points = window.data.points;
+  const points = window.data.points || [];
 
-  var map = L.map('map').setView([points[0].lat, points[0].lng], 15);
+  // Moabit
+  let initialLat = 52.52635;
+  let initialLng = 13.33903;
+
+  if (points.length) {
+     initialLat = points[0].lat;
+     initialLng = points[0].lng;
+  }
+
+  const map = L.map('map').setView([initialLat, initialLng], 15);
 
   L.tileLayer('http://a.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
 
   points.forEach((point) => {
-    console.log([point.lat, point.lng]);
     L.marker([point.lat, point.lng], {
       icon: L.icon({
         iconSize: [ 25, 41 ],
