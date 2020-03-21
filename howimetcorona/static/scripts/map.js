2fd36463ebc4d2f8c62548e91756a9024037891c
Nico Knoll <mail@nico.is>
2020-03-21 20:06:01 +0100

    Fix map height

--- howimetcorona/static/scripts/map.js
+++ howimetcorona/static/scripts/map.js
@@ -12,6 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 
   const map = L.map('map').setView([initialLat, initialLng], 15);
+  map.scrollWheelZoom.disable();
 
   L.tileLayer('http://a.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
