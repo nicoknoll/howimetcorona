4ccb7c990a2fed0be92ffbc36414044ca519c91d
Thea Kitzinger <thea@zageno.com>
2020-03-21 15:12:08 +0100

    thank you page

--- core/urls.py
+++ core/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     re_path(r'^check/?$', views.CheckView.as_view(), name='check'),
     re_path(r'^check/map/?$', views.MapView.as_view(), name='map'),
     re_path(r'^report/?$', views.ReportView.as_view(), name='report'),
+    re_path(r'^report/confirmation?$', views.ReportConfirmationView.as_view(), name='report-confirmation'),
 ]
